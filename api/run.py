78c16ce5904fd8a10ed49d5bce25994e5c59b7ef
aashrav <aashrav.shetty01@gmail.com>
2020-12-01 13:57:18 -0800

    Changed run.py

--- api/run.py
+++ api/run.py
@@ -1,4 +1,20 @@
-from app import app
+# from app import app
+from flask import Flask
+from flask_mongoengine import MongoEngine
+from flask_cors import CORS
+
+
+db = MongoEngine()
+app = Flask(__name__, static_folder='../../build', static_url_path='/')
+
+app.config['MONGODB_HOST'] = 'mongodb+srv://aashrav:qwe@skuvision.dup3c.mongodb.net/SKUVision?retryWrites=true&w=majority'
+app.config['CORS_HEADERS'] = 'Content-Type'
+cors = CORS(app)
+
+db.init_app(app)
+    
+
+from app import routing
 
 if __name__ == "__main__":
     app.run(debug=True)
