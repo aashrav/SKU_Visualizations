89056dff4d57211fa42506362e92e3f4c152e1d4
aashrav <aashrav.shetty01@gmail.com>
2020-12-01 00:30:02 -0800

    Added api stuff

---
+++ api/app/database/dbase.py
@@ -0,0 +1,11 @@
+"""
+This is where you will be creating and implementing your database
+Makes sure that any methods that you create in here builds a database
+that can be imported by the __init__.py file to connect the app with the db
+
+I don't know much about databases, so this is all you
+Feel free to read up on any Flask documentation
+"""
+
+
+
---
+++ api/app/models.py
@@ -0,0 +1,26 @@
+from app import db
+from bson.objectid import ObjectId
+
+class User(db.Document):
+  email = db.StringField(required=True)
+  name = db.StringField()
+  corporate_id = db.ObjectIdField()
+  password = db.StringField()
+
+
+class File(db.EmbeddedDocument):
+  file_ = db.FileField()
+
+class MasterFile(db.EmbeddedDocument):
+  file_composition = db.ListField(db.ObjectIdField())
+  file = db.FileField()
+
+
+class Corporate(db.Document):
+  corporate_name = db.StringField(unique = True)
+  corporate_id = db.StringField()
+
+  uploadedFiles = db.ListField(db.EmbeddedDocumentField(File), default = [])
+  masterFiles = db.ListField(db.EmbeddedDocumentField(MasterFile), default = [])
+
+
