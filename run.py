5585f76a4d86e54bd08ef38f717b9175eb061c56
aashrav <aashrav.shetty01@gmail.com>
2020-12-22 12:29:13 -0800

    changed run.py

--- run.py
+++ run.py
@@ -1,6 +1,19 @@
-import sys
+# import sys
 
-if __name__ == "__main__":
-  sys.path.insert(0, 'api')
-  from app import app
-  app.run(debug=True)
+# if __name__ == "__main__":
+#   sys.path.insert(0, 'api')
+#   from app import app
+#   app.run(debug=True)
+
+from flask import Flask
+
+my_awesome_app = Flask(__name__)
+
+
+@my_awesome_app.route('/')
+def hello_world():
+    return 'Hello World!'
+
+
+if __name__ == '__main__':
+    my_awesome_app.run()
